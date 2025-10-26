a4043e237b1580de175eebf369f3b40ae1382a98
benisprlh <benisaprulah5@gmail.com>
2025-10-26 13:56:13 +0700

    Day 1

---
+++ contracts/LearnNumber.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract LearnNumber {
+    // Angka untuk data tanaman
+    uint256 public plantId;
+    uint256 public waterLevel;
+
+    constructor() {
+        plantId = 1;
+        waterLevel = 100;
+    }
+
+    function changePlantId(uint256 _newId) public {
+        plantId = _newId;
+    }
+
+    function addWater() public {
+        waterLevel = waterLevel + 10;
+        // Bisa juga ditulis: waterLevel += 10;
+    }
+
+    function reduceWater() public {
+        waterLevel = waterLevel - 10;
+        // Bisa juga ditulis: waterLevel += 10;
+    }
+}
